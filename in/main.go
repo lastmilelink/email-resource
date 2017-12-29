21b37e187dd38ad97874b286e7deb05b4911fd59
Dionisio Perez-Mavrogenis <dperezmavrogenis@lastmilelink.com>
2017-12-29 15:52:42 +0000

    added in resource that does  nothing PLS-721

--- in/main.go
+++ in/main.go
@@ -1,7 +1,37 @@
 package main
 
-import "fmt"
+import (
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"os"
+)
+
+func checkError(err error, errMsg string) {
+	if err != nil {
+		fmt.Fprintln(os.Stderr, errMsg)
+		os.Exit(1)
+	}
+}
 
 func main() {
-	fmt.Println("[]")
+	var output struct {
+		Version interface{} `json:"version"`
+	}
+
+	stdinData, err := ioutil.ReadAll(os.Stdin)
+	checkError(err, fmt.Sprintf("error reading from stdin: %v", err))
+
+	err = json.Unmarshal(stdinData, &output)
+	checkError(err, fmt.Sprintf("error unmarshalling JSON: %v", err))
+
+	if output.Version == nil {
+		fmt.Fprintf(os.Stderr, "error: version key pair is missing from stdin")
+		os.Exit(1)
+	}
+
+	stdoutOutput, err := json.Marshal(output)
+	checkError(err, fmt.Sprintf("error marshalling output for stdout: %v", err))
+
+	fmt.Printf("%s", []byte(stdoutOutput))
 }
