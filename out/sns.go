592ea7373f7e8877bd1b532d8f3faff675bb9fb7
Dionisio Perez-Mavrogenis <dperezmavrogenis@lastmilelink.com>
2018-01-03 14:12:46 +0000

    PLS-721 cleared up output

--- out/sns.go
+++ out/sns.go
@@ -89,11 +89,10 @@ func (s *snsClient) createSubscription(endpoint string) (*sns.SubscribeOutput, e
 	)
 }
 
-func (s *snsClient) subscribe(subscribers []string) (map[string]string, error) {
-	var result = make(map[string]string)
+func (s *snsClient) subscribe(subscribers []string) error {
 	subscriptions, err := s.listSubscriptionsByTopic()
 	if err != nil {
-		return nil, fmt.Errorf("Unable to list subscriptionss by topic: %v", err)
+		return fmt.Errorf("Unable to list subscriptions by topic: %v", err)
 	}
 
 	var endpoints = make(map[string]bool)
@@ -107,14 +106,13 @@ func (s *snsClient) subscribe(subscribers []string) (map[string]string, error) {
 			logln(fmt.Sprintf("[*] Creating subscription for %s", v))
 			res, err := s.createSubscription(v)
 			if err != nil {
-				return nil, fmt.Errorf("Error creating subscriptions for %s :%v", v, err)
+				return fmt.Errorf("Error creating subscriptions for %s :%v", v, err)
 			}
-			logln(fmt.Sprintf("[*] SubscriptionArn is %s", *res.SubscriptionArn))
-			result[v] = *res.SubscriptionArn
+			logln(fmt.Sprintf("    subscriptionArn is %s", *res.SubscriptionArn))
 		}
 	}
 
-	return result, nil
+	return nil
 }
 
 func (s *snsClient) publish(params Parameters) (*sns.PublishOutput, error) {
@@ -126,12 +124,9 @@ func (s *snsClient) publish(params Parameters) (*sns.PublishOutput, error) {
 	logf("[+] Created topic %s\n", *output.TopicArn)
 
 	logf("[*] Adding subscriptions for %s\n", strings.Join(params.Subscribers, ", "))
-	subscriptions, err := s.subscribe(params.Subscribers)
+	err = s.subscribe(params.Subscribers)
 	if err != nil {
 		return nil, fmt.Errorf("Error when calling subscribe: %v", err)
-	}
-	for k, v := range subscriptions {
-		logln(fmt.Sprintf("Subscription: %s: %s", k, v))
 	}
 
 	pOutput, err := s.publishMessage(params.EmailSubject, params.EmailBody)
