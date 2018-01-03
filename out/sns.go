5b53e78599ef20b769abea7da7bb4cde48ad2400
Dionisio Perez-Mavrogenis <dperezmavrogenis@lastmilelink.com>
2018-01-03 12:49:17 +0000

    adding logic for performing subscriptions

--- out/sns.go
+++ out/sns.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"strings"
 
 	"github.com/aws/aws-sdk-go/aws"
 	"github.com/aws/aws-sdk-go/aws/session"
@@ -15,6 +16,8 @@ type snsClient struct {
 	topicArn         string
 	topicName        string
 }
+
+var protocol string = "email"
 
 func newSnsClient(config awsConfiguration) snsClient {
 	sess := session.Must(
@@ -50,6 +53,70 @@ func (s *snsClient) publishMessage(subject, body string) (*sns.PublishOutput, er
 	return s.snsService.Publish(&input)
 }
 
+func (s *snsClient) createListSubscriptionsInput(next *string) *sns.ListSubscriptionsByTopicInput {
+	return &sns.ListSubscriptionsByTopicInput{
+		TopicArn:  &s.topicArn,
+		NextToken: next,
+	}
+}
+
+func (s *snsClient) listSubscriptionsByTopic() ([]*sns.Subscription, error) {
+	var result []*sns.Subscription
+
+	response, err := s.snsService.ListSubscriptionsByTopic(s.createListSubscriptionsInput(nil))
+	if err != nil {
+		return nil, fmt.Errorf("Unable to perform first call to listSubscriptionsByTopic: %v", err)
+	}
+
+	for response.NextToken != nil {
+		result = append(result, response.Subscriptions...)
+		response, err = s.snsService.ListSubscriptionsByTopic(s.createListSubscriptionsInput(nil))
+		if err != nil {
+			return nil, fmt.Errorf("Unable to perform call to listSubscriptionsByTopic: %v", err)
+		}
+	}
+
+	return result, nil
+}
+
+func (s *snsClient) createSubscription(endpoint string) (*sns.SubscribeOutput, error) {
+	return s.snsService.Subscribe(
+		&sns.SubscribeInput{
+			Endpoint: &endpoint,
+			Protocol: &protocol,
+			TopicArn: &s.topicArn,
+		},
+	)
+}
+
+func (s *snsClient) subscribe(subscribers []string) (map[string]string, error) {
+	var result = make(map[string]string)
+	subscriptions, err := s.listSubscriptionsByTopic()
+	if err != nil {
+		return nil, fmt.Errorf("Unable to list subscriptionss by topic: %v", err)
+	}
+
+	var endpoints = make(map[string]bool)
+
+	for _, v := range subscriptions {
+		endpoints[*v.Endpoint] = true
+	}
+
+	for _, v := range subscribers {
+		if !endpoints[v] {
+			logln(fmt.Sprintf("[*] Creating subscription for %s", v))
+			res, err := s.createSubscription(v)
+			if err != nil {
+				return nil, fmt.Errorf("Error creating subscriptions for %s :%v", v, err)
+			}
+			logln(fmt.Sprintf("[*] SubscriptionArn is %s", *res.SubscriptionArn))
+			result[v] = *res.SubscriptionArn
+		}
+	}
+
+	return result, nil
+}
+
 func (s *snsClient) publish(params Parameters) (*sns.PublishOutput, error) {
 	output, err := s.createTopic()
 	if err != nil {
@@ -57,6 +124,15 @@ func (s *snsClient) publish(params Parameters) (*sns.PublishOutput, error) {
 	}
 	s.topicArn = *output.TopicArn
 	logf("[+] Created topic %s\n", *output.TopicArn)
+
+	logf("[*] Adding subscriptions for %s\n", strings.Join(params.Subscribers, ", "))
+	subscriptions, err := s.subscribe(params.Subscribers)
+	if err != nil {
+		return nil, fmt.Errorf("Error when calling subscribe: %v", err)
+	}
+	for k, v := range subscriptions {
+		logln(fmt.Sprintf("Subscription: %s: %s", k, v))
+	}
 
 	pOutput, err := s.publishMessage(params.EmailSubject, params.EmailBody)
 	if err != nil {
